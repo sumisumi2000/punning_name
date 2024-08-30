e4a1526ed5dc59ad1027503a78003170be4f37e3
sumisumi2000 <sumio.vantan308@gmail.com>
2024-08-31 00:47:28 +0900

    問題を事前に作成する形に変更

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,33 +1,36 @@
 class QuizzesController < ApplicationController
-  before_action :set_teams, only: %i[new]
+  before_action :reset_quiz, only: %i[new]
 
   # 問題数
-  QUIZ_NUM = 5
+  QUIZ_NUM = 7
+  # kaisei フォントの問題
+  KAISEI = [ 1, 3, 5 ]
 
   def new
-    # 1 ~ 20 の中からランダムな数を生成
-    team_id = (1..20).to_a.sample
-    # JOne テーブルから無作為にチームを１つ選択
-    team = JOne.find team_id
-
-    # 選択肢を生成
-    @choices = [ team.name, team.name.sub("ド", "ト"), team.name.sub("ー", "一"), team.name.sub("ガ", "カ") ]
-
-    # セッションに選択肢のチームを保存
-    session[:used_team_ids] ||= []
-    session[:used_team_ids] << team.id
     # セッションに問題数を保存
     session[:quiz_count] ||= 0
     session[:quiz_count] += 1
+
+    # 問題に応じてフォントを設定
+    @font_name = KAISEI.include?(session[:quiz_count])
+
+    # 選択肢を生成
+    # 1 -> 1 ~ 4
+    # 2 -> 5 ~ 8
+    # 3 -> 9 ~ 12
+    # 4 -> 13 ~ 16
+    # 5 -> 17 ~ 20
+    @choices = Quiz.where(id: (session[:quiz_count] * 4 - 3)..session[:quiz_count] * 4).map(&:name)
   end
 
   def check
-    p JOne.find(session[:used_team_ids].last)
     # 正誤判定
-    @is_answer_correct = params[:name] == JOne.find(session[:used_team_ids].last).name
-
+    @is_answer_correct = Answer.find_by(name: params[:name])
+    # 正解数をカウント
+    session[:correct_count] ||= 0
+    session[:correct_count] += 1 if @is_answer_correct
     # 次の問題があるか判定
-    @has_next_question = session[:quiz_count] < 5
+    @has_next_question = session[:quiz_count] < QUIZ_NUM
   end
 
   def destroy
@@ -38,12 +41,9 @@ class QuizzesController < ApplicationController
   end
 
   private
-  def set_teams
-    # 1問目の場合は配列が空なので終了
-    return if session[:used_team_ids].nil?
-    # 選択可能なチームの配列を格納するセッションが未作成なら、作成
-    session[:available_team_ids] ||= (1..20).to_a
-    # 選択可能なチームから使用済みのチームの id を削除
-    session[:available_team_ids] = session[:available_team_ids].difference(session[:used_team_ids])
+
+  def reset_quiz
+    session[:quiz_count] ||= 0
+    reset_session if session[:quiz_count] >= QUIZ_NUM
   end
 end
