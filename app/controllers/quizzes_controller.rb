6e75b3dd1b30cfe0bc7d11157cdba71d33730b37
sumisumi2000 <sumio.vantan308@gmail.com>
2024-08-31 12:19:32 +0900

    回答後にラジオボタンを選択不能にする

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -11,21 +11,18 @@ class QuizzesController < ApplicationController
     session[:quiz_count] ||= 0
     session[:quiz_count] += 1
 
+    # 選択肢を設定
+    @choices = get_choices
+
     # 問題に応じてフォントを設定
     @font_name = KAISEI.include?(session[:quiz_count])
-
-    # 選択肢を生成
-    # 1 -> 1 ~ 4
-    # 2 -> 5 ~ 8
-    # 3 -> 9 ~ 12
-    # 4 -> 13 ~ 16
-    # 5 -> 17 ~ 20
-    @choices = Quiz.where(id: (session[:quiz_count] * 4 - 3)..session[:quiz_count] * 4).map(&:name).shuffle
   end
 
   def check
+    # ユーザーが選んだ選択肢から選択肢番号と選択肢の値に分割
+    @choice_number, user_choice = params[:user_choice]&.split("-")
     # 正誤判定
-    @is_answer_correct = Answer.find_by(name: params[:name])
+    @is_answer_correct = Answer.find_by(name: user_choice)
     # 正解数をカウント
     session[:correct_count] ||= 0
     session[:correct_count] += 1 if @is_answer_correct
@@ -42,6 +39,17 @@ class QuizzesController < ApplicationController
 
   private
 
+  # 現在の問題数に対応する選択肢をセット
+  # 1 -> 1 ~ 4
+  # 2 -> 5 ~ 8
+  # 3 -> 9 ~ 12
+  # 4 -> 13 ~ 16
+  # 5 -> 17 ~ 20
+  def get_choices
+    Quiz.where(id: (session[:quiz_count] * 4 - 3)..session[:quiz_count] * 4).map(&:name).shuffle
+  end
+
+  # クイズが問題上限に達したらセッションをリセット
   def reset_quiz
     session[:quiz_count] ||= 0
     reset_session if session[:quiz_count] >= QUIZ_NUM
