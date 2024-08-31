7a26a6020f5c938b964cdd156632aa9225738d05
sumisumi2000 <sumio.vantan308@gmail.com>
2024-08-31 16:06:39 +0900

    X シェアの時のメッセージを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def generate_congratulations
+    case session[:correct_count]
+    when 1
+      "いいスタートですね！次も頑張ってください！"
+    when 2
+      "いい調子ですね！さらに進んでいきましょう！"
+    when 3
+      "順調ですね！この調子でいきましょう！"
+    when 4
+      "パーフェクトが見えてきましたね！もう少しです！"
+    when 5
+      "素晴らしいです！パーフェクトまでもう直ぐです！"
+    when 6
+      "あと一歩で満点です！最後のひと踏ん張りです！"
+    when 7
+      "全ての問題を正解しました！最高のパフォーマンスです！"
+    end
+  end
 end
